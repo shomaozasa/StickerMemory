414906445ce86ede8fe9a9413f0d39239b1ab713
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-03-11 17:05:08 +0900

    ファイル選択機能実装

--- sticker_memory/lib/pages/sticker_register.dart
+++ sticker_memory/lib/pages/sticker_register.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
+class StickerRegister extends StatefulWidget {
+  @override
+  _StickerRegisterState createState() => _StickerRegisterState();
+}
+
+class _StickerRegisterState extends State<StickerRegister> {
+  String? _fileName;
+
+  Future<void> _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      setState(() {
+        _fileName = result.files.first.name;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Sticker Register')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(onPressed: _pickFile, child: Text('ファイルを選択')),
+            SizedBox(height: 20),
+            if (_fileName != null) Text('選択されたファイル: $_fileName'),
+          ],
+        ),
+      ),
+    );
+  }
+}
