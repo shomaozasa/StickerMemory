b63bf8a41619db9e2cc17fe565492187cdb9844d
shinnosukegoto <xinzhizhuw@gmail.com>
2025-05-30 17:09:50 +0900

    sticker登録

--- sticker_memory/lib/pages/registration_sticker.dart
+++ sticker_memory/lib/pages/registration_sticker.dart
@@ -1,11 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:sticker_memory/pages/custom_appbar.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ImagePickerPage extends StatefulWidget {
   @override
@@ -14,6 +13,9 @@ class ImagePickerPage extends StatefulWidget {
 
 class _ImagePickerPageState extends State<ImagePickerPage> {
   File? _selectedImage;
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _memoController = TextEditingController();
+  final user = FirebaseAuth.instance.currentUser!;
 
   Future<void> _pickImage() async {
     final picker = ImagePicker();
@@ -23,21 +25,34 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
       setState(() {
         _selectedImage = File(image.path);
       });
-      File file = File(image.path);
-      FirebaseStorage storage = FirebaseStorage.instance;
-      try {
-        // Firebase Storageに画像をアップロード
-        await storage
-            .ref('stickers/${file.uri.pathSegments.last}')
-            .putFile(file);
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text('画像がアップロードされました')));
-      } catch (e) {
-        ScaffoldMessenger.of(
-          context,
-        ).showSnackBar(SnackBar(content: Text('アップロードエラー: ${e.toString()}')));
-      }
+    }
+  }
+
+  Future<void> _uploadImageToFirebase(File file) async {
+    FirebaseStorage storage = FirebaseStorage.instance;
+    final ref = storage.ref(
+      'stickers/${user.displayName}/${file.uri.pathSegments.last}',
+    );
+    try {
+      // Firebase Storageに画像をアップロード
+      await ref.putFile(file);
+      final imageUrl = await ref.getDownloadURL();
+      // Firestoreに画像のURLを保存
+      await FirebaseFirestore.instance.collection('stickers').add({
+        'userId': user.uid,
+        'title': _titleController.text,
+        'memo': _memoController.text,
+        'date': Timestamp.now(),
+        'imageUrl': imageUrl,
+      });
+      // アップロード成功のメッセージ
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('画像がアップロードされました')));
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('アップロードエラー: ${e.toString()}')));
     }
   }
 
@@ -57,8 +72,24 @@ class _ImagePickerPageState extends State<ImagePickerPage> {
                   fit: BoxFit.cover,
                 )
                 : Text('画像が選択されていません'),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'ステッカー名'),
+            ),
+            TextField(
+              controller: _memoController,
+              decoration: InputDecoration(labelText: 'メモ'),
+            ),
+            const SizedBox(height: 20),
             ElevatedButton(onPressed: _pickImage, child: Text('画像を選択')),
+            ElevatedButton(
+              onPressed:
+                  _selectedImage != null
+                      ? () => _uploadImageToFirebase(_selectedImage!)
+                      : null,
+              child: Text('ステッカーを保存する'),
+            ),
           ],
         ),
       ),
